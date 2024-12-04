7612aee0447788cab5e5943e5e043358a7955bfa
Igor Moura <igor.sousamoura@outlook.com>
2024-12-03 23:17:54 -0300

    feat-inicializando-projeto

---
+++ src/main/java/com/example/sistema_bbb/SistemaBbbApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistema_bbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBbbApplication.class, args);
+	}
+
+}
